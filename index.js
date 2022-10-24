70c53bb7c7385230cc34de38d3145193c15214cf
Naimcoder <isteiaknaim12@gmail.com>
2022-10-24 21:15:24 +0600

    added my server express variable

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // https://coures-express-server.vercel.app
+const express = require('express')
+const app= express()
+const cors= require('cors')
+const port= process.env.Port ||5000;
+app.use(cors())
