17220d4f8b3ecdffbb3d5380391fccd6325a97f8
Naimcoder <isteiaknaim12@gmail.com>
2022-10-24 21:16:10 +0600

    added all items server express

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const app= express()
 const cors= require('cors')
 const port= process.env.Port ||5000;
 app.use(cors())
+app.get('/',(req,res)=>{
+    app.sen('hello my news')
+})
+app.listen(port,()=>{
+    console.log('my server')
+})
