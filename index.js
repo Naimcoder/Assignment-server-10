62fb51cd1e495e0f62be632dfd6cc6edf1c90c94
Naimcoder <isteiaknaim12@gmail.com>
2022-10-25 16:21:34 +0600

    added data json file create

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const port= process.env.Port ||5000;
 
 app.use(cors())
 
+const courses=require('./Data/Course.json')
 
 app.get('/',(req,res)=>{
     app.sen('hello my news')
 })
 
+app.get('/course',(req,res)=>{
+    res.send(courses)
+})
+app.get('/course/:id',(req,res)=>{
+    const id = req.params.id
+    const selectedCourse=course.find(Sc=>Sc.id===id)
+    res.send(selectedCourse)
+})
 
 app.listen(port,()=>{
     console.log('my server')
