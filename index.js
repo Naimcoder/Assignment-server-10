6bb3fe750ebda40cd729e90b39b8984216bc055f
Naimcoder <isteiaknaim12@gmail.com>
2022-10-25 16:30:27 +0600

    all part added

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port= process.env.Port ||5000;
 
 app.use(cors())
 
-const courses=require('./Data/Course.json')
+const courses= require('./Data/Course.json')
+const allCourse = require('./Data/Course_Caregory.json')
 
 app.get('/',(req,res)=>{
     app.sen('hello my news')
@@ -21,6 +22,15 @@ app.get('/course/:id',(req,res)=>{
     const selectedCourse=course.find(Sc=>Sc.id===id)
     res.send(selectedCourse)
 })
+
+app.get('/allcourse/:id',(req,res)=>{
+  const id= req.params.id
+  const selectedSingleCourse=allCourse.find(allCourse=>allCourse.id===id)
+  res.send(selectedSingleCourse)
+})
+ app.get('/allcourse',(req,res)=>{
+    res.send(allCourse)
+ })
 
 app.listen(port,()=>{
     console.log('my server')
